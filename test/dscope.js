e03985a46205c165f96e24ba316a2d8c702c5bde
Mariusz Nowak <mariusz@medikoo.com>
2012-02-20 12:56:47 +0100

    dscope was removed from es5-ext, for the meantime take it into domjs

---
+++ test/dscope.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var f, o, org;
+
+org = decodeURI;
+f = function () {
+	return c;
+};
+o = { c: {}, decodeURI: true };
+
+module.exports = function (t, a) {
+	a(t(f, o), o.c, "Dynamic scope");
+
+	t(f, o);
+	a(decodeURI, org, "Reverts global properties");
+};
