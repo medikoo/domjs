719af21cca761a09b457c53793290c4e321320df
Mariusz Nowak <mariusz@medikoo.com>
2013-08-07 22:12:01 +0200

    -- lint

--- test/__tad.js
+++ test/__tad.js
@@ -5,6 +5,6 @@ var document;
 try {
 	document = new (require('jsdom/lib/jsdom/level3/core')
 		.dom.level3.core.Document)();
-} catch (e) {}
+} catch (ignore) {}
 
 exports.context = document ? { document: document } : {};
