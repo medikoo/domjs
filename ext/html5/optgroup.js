57ce8603c0f61287c26a648e3e2c7ea2483b7ecb
Mariusz Nowak <mariusz@medikoo.com>
2013-09-13 11:41:31 +0200

    List handling for optgroup elements

---
+++ ext/html5/optgroup.js
@@ -0,0 +1,4 @@
+'use strict';
+
+exports._construct = require('./_list-construct')('option',
+	require('dom-ext/html-option-element/is-html-option-element'));
--- index.js
+++ index.js
@@ -48,9 +48,10 @@ HTML5.prototype = Object.create(Base.prototype, extend({
 }));
 
 extend(require('./ext'), {
-	ol:     require('./ext/html5/ol'),
-	script: require('./ext/html5/script'),
-	select: require('./ext/html5/select'),
-	tbody:  require('./ext/html5/tbody'),
-	ul:     require('./ext/html5/ul')
+	ol:       require('./ext/html5/ol'),
+	optgroup: require('./ext/html5/optgroup'),
+	script:   require('./ext/html5/script'),
+	select:   require('./ext/html5/select'),
+	tbody:    require('./ext/html5/tbody'),
+	ul:       require('./ext/html5/ul')
 });
