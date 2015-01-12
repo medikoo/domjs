b240c1fe86cbcbfd34ecb09660dffd10e772e1fb
Mariusz Nowak <medyk@medikoo.com>
2015-01-12 14:59:30 +0100

    -- whitespace

--- ext/html5/script.js
+++ ext/html5/script.js
@@ -3,7 +3,7 @@
 var isFunction     = require('es5-ext/function/is-function')
   , isPlainObject  = require('es5-ext/object/is-plain-object')
   , castAttributes = require('dom-ext/element/#/cast-attributes')
-  , genScript    = require('dom-ext/html-document/generate-inline-script-text');
+  , genScript      = require('dom-ext/html-document/generate-inline-script-text');
 
 exports._construct = function (attrs, src) {
 	if (isFunction(attrs)) src = genScript.apply(null, arguments);
