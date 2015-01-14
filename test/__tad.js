b3edc3474c69345691f00751213ef6b685616571
Mariusz Nowak <medyk@medikoo.com>
2015-01-14 16:46:30 +0100

    -- whitespace

--- test/__tad.js
+++ test/__tad.js
@@ -3,8 +3,7 @@
 var document;
 
 try {
-	document = new (require('jsdom/lib/jsdom/level3/core')
-		.dom.level3.core.Document)();
+	document = new (require('jsdom/lib/jsdom/level3/core').dom.level3.core.Document)();
 } catch (ignore) {}
 
 exports.context = document ? { document: document } : {};
