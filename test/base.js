64c06485e86577c4dd7c7beb674b248c72577684
Mariusz Nowak <medyk@medikoo.com>
2015-04-15 14:31:43 +0200

    Update tests to work with newer jsdom

--- test/base.js
+++ test/base.js
@@ -34,19 +34,19 @@ module.exports = function (t, a) {
 	a(isDF(dom), true, "Expect document fragment");
 
 	dom = dom.firstChild;
-	a(dom.nodeName, 'foo', "Match firstChild");
+	a(dom.nodeName.toLowerCase(), 'foo', "Match firstChild");
 	a(dom.childNodes.length, 1, "ChildNodes length");
 	a(dom.firstChild.nodeType, 3, "Append text node");
 	a(dom.firstChild.data, 'foo text', "Append text node content");
 
 	dom = dom.nextSibling;
 	a(dom.nodeType, 1, "Sibling element");
-	a(dom.nodeName, 'bar', "Sibling element name");
+	a(dom.nodeName.toLowerCase(), 'bar', "Sibling element name");
 	a(dom.getAttribute('class'), 'test-class', "Element attribute");
 	a(dom.getAttribute('other'), 'test-other', "Element other attribute");
 	a(dom.childNodes.length, 5, "Late children");
 	a(dom.firstChild.nodeType, 1, "Element child type");
-	a(dom.firstChild.nodeName, 'foo', "Element child name");
+	a(dom.firstChild.nodeName.toLowerCase(), 'foo', "Element child name");
 	a(dom.lastChild.textContent, 'pięć', "Late child content");
 	a(dom.getAttribute('foo'), 'bar', "Direct attribute");
 
--- test/index.js
+++ test/index.js
@@ -22,18 +22,18 @@ module.exports = function (t, a) {
 	a(isDF(dom), true, "Expect document fragment");
 
 	dom = dom.firstChild;
-	a(dom.nodeName, 'div', "Match firstChild");
+	a(dom.nodeName.toLowerCase(), 'div', "Match firstChild");
 	a(dom.childNodes.length, 1, "ChildNodes length");
 	a(dom.firstChild.nodeType, 3, "Append text node");
 	a(dom.firstChild.data, 'foo text', "Append text node content");
 
 	dom = dom.nextSibling;
 	a(dom.nodeType, 1, "Sibling element");
-	a(dom.nodeName, 'p', "Sibling element name");
+	a(dom.nodeName.toLowerCase(), 'p', "Sibling element name");
 	a(dom.getAttribute('class'), 'test-class', "Element attribute");
 	a(dom.getAttribute('other'), 'test-other', "Element other attribute");
 	a(dom.childNodes.length, 3, "Children");
 	a(dom.firstChild.nodeType, 1, "Element child type");
-	a(dom.firstChild.nodeName, 'span', "Element child name");
+	a(dom.firstChild.nodeName.toLowerCase(), 'span', "Element child name");
 	a(dom.lastChild.textContent, 'trzy', "Last child content");
 };
