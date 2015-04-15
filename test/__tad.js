9a2dfc811eb1ee8d197176c1ea110589b54a7e6f
Mariusz Nowak <medyk@medikoo.com>
2015-04-15 14:26:35 +0200

    Use latest version of jsdom

--- test/__tad.js
+++ test/__tad.js
@@ -3,7 +3,7 @@
 var document;
 
 try {
-	document = new (require('jsdom/lib/jsdom/level3/core').dom.level3.core.Document)();
+	document = require('jsdom').jsdom();
 } catch (ignore) {}
 
 exports.context = document ? { document: document } : {};
